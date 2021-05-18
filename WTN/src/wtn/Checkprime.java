35c552957d698972f08daf6c941cb74a8288bfdc
potrusaikiran <saikiransudharani@gmail.com>
2021-05-18 22:06:18 +0530

    wtn practise

---
+++ WTN/src/wtn/Checkprime.java
@@ -0,0 +1,25 @@
+package wtn;
+import java.util.*;
+public class Checkprime {
+	public static void main(String args[])
+	{
+		int num;
+		Scanner sc=new Scanner(System.in);
+		num=sc.nextInt();
+		boolean flag = false;
+	    for (int i = 2; i <= num / 2; ++i) {
+	      if (num % i == 0) {
+	        flag = true;
+	        break;
+	      }
+	    }
+
+	    if (!flag)
+	      System.out.println(num + " is a prime number.");
+	    else
+	      System.out.println(num + " is not a prime number.");
+		
+		sc.close();
+	}
+
+}
